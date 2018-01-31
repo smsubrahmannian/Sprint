8b2361770fb2e6e5a6e27cf2d9b633967540270b
NIMESH SINHA <nimesh@NIMESHs-MacBook-Pro.local>
2018-01-31 00:55:33 -0800

    updated

--- sprint2_step2.py
+++ sprint2_step2.py
@@ -1,11 +1,15 @@
 directory = "srv/runme/"
 prefix = "t"
 
-import os, json
-
-json_files = [x for x in os.listdir(directory) if x.startswith(prefix)]
-
-for item in json_files:
-    with open(os.path.join(directory, item)) as file:
-        json_text = json.load(file)
-        print (json_text['name'] + "\t" + str(json_text['prop']['age']))
+def prefix(directory,prefix):
+    json_files = [x for x in os.listdir(directory) if x.startswith(prefix)  ]
+    with open(directory+str(prefix)+'.txt','w') as text_file:
+       
+        for item in json_files:
+        
+            with open(os.path.join(directory, item)) as file:
+                
+                for line in file:
+                    json_text = json.loads(line)
+                    text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age'])+'\n')
+    text_file.close()
