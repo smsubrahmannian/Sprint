42486c943043154f6a78674e289f65caf9b77c88
nsinha280 <nsinha2@dons.usfca.edu>
2018-01-31 01:12:00 -0800

    updated

--- sprint2_step2.py
+++ sprint2_step2.py
@@ -1,7 +1,7 @@
-directory = "srv/runme/"
-prefix = "t"
+
 
 def prefix(directory,prefix):
+    if directory == "srv/sunme/":
     json_files = [x for x in os.listdir(directory) if x.startswith(prefix)  ]
     with open(directory+str(prefix)+'.txt','w') as text_file:
        
@@ -13,3 +13,5 @@ def prefix(directory,prefix):
                     json_text = json.loads(line)
                     text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age'])+'\n')
     text_file.close()
+
+prefix("srv/runme/",prefix)
