20561d03b729fcf8b7ae0a1c0ff92b576c1768e8
Alvira Swalin <alvira@Alvira.local>
2018-01-30 23:40:05 -0800

    step2

--- sprint2_step2.py
+++ sprint2_step2.py
@@ -8,5 +8,4 @@ json_files = [x for x in os.listdir(directory) if x.startswith(prefix)]
 for item in json_files:
     with open(os.path.join(directory, item)) as file:
         json_text = json.load(file)
-        print(json_text)
         print (json_text['name'] + "\t" + str(json_text['prop']['age']))
