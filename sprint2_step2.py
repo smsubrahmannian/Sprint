45bf564bc121d9ed6d445138f397ad6c77c14da4
maisely <tly10@dons.usfca.edu>
2018-02-01 13:44:29 -0800

    add wrapper for json write function

--- sprint2_step2.py
+++ sprint2_step2.py
@@ -1,20 +1,35 @@
+import os
+import json
+import sys
+from os.path import expanduser
+
+DIRECTORY = "/srv/runme/"
+full_dir = expanduser("~")+DIRECTORY
+
+def proc_line(line, text_file):
+
+    json_text = json.loads(line)
+    try:
+        text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age']) + '\n')
+    except:
+        pass
 
 
-def prefix(directory,prefix):
-    if directory == "srv/sunme/":
-        json_files = [x for x in os.listdir(directory) if x.startswith(prefix)  ]
-        with open(directory+str(prefix)+'.txt','w') as text_file:
-        
-            for item in json_files:
-            
-                with open(os.path.join(directory, item)) as file:
-                    
-                    for line in file:
-                        json_text = json.loads(line)
-                        try:
-                            text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age'])+'\n')
-                        except:
-                            pass
-        text_file.close()
+def write_json(prefix):
 
-prefix("srv/runme/",prefix)
+    json_files = [ x for x in os.listdir(full_dir) if x.startswith(prefix) ]
+    with open(full_dir+str(prefix)+'.txt','w') as text_file:
+
+        for item in json_files:
+
+            with open(os.path.join(full_dir, item)) as file:
+
+                for line in file:
+                    try: proc_line(line, text_file)
+                    except: pass
+
+    text_file.close()
+
+# run prefix
+PREFIX = sys.argv[1]
+write_json(PREFIX)
