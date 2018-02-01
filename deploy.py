370e25621eef1f69958ca1d33610db2137b62906
Davi-Schumacher <daschumacher@dons.usfca.edu>
2018-02-01 14:42:12 -0800

    updated deploy with sys args and new ec2 commands

---
+++ deploy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import paramiko
+import time
+import sys
+
+# #Change these for your instance
+# key_path = '/Users/davischum/Documents/classes/aws/sprint.pem'
+# host = 'ec2-54-191-242-17.us-west-2.compute.amazonaws.com'
+
+key_path = sys.argv[1]
+host = sys.argv[2]
+# prefix = sys.argv[2]
+user = 'ec2-user'
+
+print "Connecting to box"
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=host, username=user, key_filename=key_path)
+print 'Connected\n'
+time.sleep(1)
+
+print 'Running commands\n'
+time.sleep(1)
+
+commands = ["if [ ! -d 'sprint' ]; then git clone https://github.com/smsubrahmannian/Sprint.git sprint; fi",
+			"crontab -l > mycron", "echo '* * * * * python sprint/sprint2_step2.py t' >> mycron",
+			"crontab mycron"]
+
+for command in commands:
+	print command
+	stdin, stdout, stderr = ssh.exec_command(command)
+	out, err = (stdout.read(), stderr.read())
+	if len(out) > 0:
+		print out
+	if len(err) > 0:
+		print err
+	time.sleep(1)
+
+ssh.close()
+## EOF ##
