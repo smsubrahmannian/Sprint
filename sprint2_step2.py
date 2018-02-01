89d84c2db725a1b1ca3d221963964a17ff5f5801
nsinha280 <nsinha2@dons.usfca.edu>
2018-01-31 18:25:01 -0800

    updated

--- sprint2_step2.py
+++ sprint2_step2.py
@@ -2,16 +2,19 @@
 
 def prefix(directory,prefix):
     if directory == "srv/sunme/":
-    json_files = [x for x in os.listdir(directory) if x.startswith(prefix)  ]
-    with open(directory+str(prefix)+'.txt','w') as text_file:
-       
-        for item in json_files:
+        json_files = [x for x in os.listdir(directory) if x.startswith(prefix)  ]
+        with open(directory+str(prefix)+'.txt','w') as text_file:
         
-            with open(os.path.join(directory, item)) as file:
-                
-                for line in file:
-                    json_text = json.loads(line)
-                    text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age'])+'\n')
-    text_file.close()
+            for item in json_files:
+            
+                with open(os.path.join(directory, item)) as file:
+                    
+                    for line in file:
+                        json_text = json.loads(line)
+                        try:
+                            text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age'])+'\n')
+                        except:
+                            pass
+        text_file.close()
 
 prefix("srv/runme/",prefix)
