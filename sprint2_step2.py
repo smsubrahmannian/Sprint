7a5fff353770748fedae5c7a92590d83cc664902
maisely <tly10@dons.usfca.edu>
2018-02-01 18:24:40 -0800

    added filter for empty string

--- sprint2_step2.py
+++ sprint2_step2.py
@@ -5,20 +5,30 @@ from os.path import expanduser
 
 
 def proc_line(line, text_file):
-
+    """
+    Extract name and age property from each json blob
+    Write them to the given file
+    """
     json_text = json.loads(line)
     try:
-        text_file.write(str(json_text['name']) + "\t" + str(json_text['prop']['age']) + '\n')
-    except:
-        pass
+        name = json_text['name']
+        age = json_text['prop']['age']
+        if name != '' and age != '':
+            text_file.write(str(name) + "\t" + str(age) + '\n')
+        else: pass
+
+    except: pass
 
 
 def write_json(prefix):
-
+    """
+    Collect all json files in the directory and process them using proc_line
+    """
     json_files = [ x for x in os.listdir(PATH) if x.startswith(prefix) ]
-    with open(PATH+str(prefix)+'.txt','w') as text_file:
 
-        for item in json_files:
+    with open(PATH+str(prefix)+'.txt','w') as text_file: # open file to write
+
+        for item in json_files: # process each file
 
             with open(os.path.join(PATH, item)) as file:
 
@@ -30,7 +40,8 @@ def write_json(prefix):
 
 # run prefix
 dir = "/srv/runme/"
-PATH = expanduser("~")+ dir
-
+PATH = expanduser("~") + dir
 PREFIX = sys.argv[1]
+
 write_json(PREFIX)
+
