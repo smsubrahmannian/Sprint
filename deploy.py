17f85b059ccbb478d70c36aa834c5ef3bef27ab3
maisely <tly10@dons.usfca.edu>
2018-02-05 23:25:44 -0800

    add exception handler for missing inputs and connection errors

--- deploy.py
+++ deploy.py
@@ -4,45 +4,54 @@ import paramiko
 import time
 import sys
 
-# #Change these for your instance
+# Change these for your instance
 # key_path = '/Users/davischum/Documents/classes/aws/sprint.pem'
 # host = 'ec2-54-191-242-17.us-west-2.compute.amazonaws.com'
-
-key_path = sys.argv[1]
-host = sys.argv[2]
-prefix = sys.argv[3]
-user = 'ec2-user'
 
 def deploy(key_path, host, prefix):
 
 	print "Connecting to box"
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(hostname=host, username=user, key_filename=key_path)
-	print 'Connected\n'
-	time.sleep(1)
+	try:
+                ssh.connect(hostname=host, username=user, key_filename=key_path)
+                print 'Connected\n'
+                time.sleep(1)
 
-	print 'Running commands\n'
-	time.sleep(1)
+                print 'Running commands\n'
+                time.sleep(1)
 
-	crontab_command = "echo '* * * * * python sprint/sprint2_step2.py "+prefix+"' >> mycron"
-	commands = ["if [ ! -d 'sprint' ]; then git clone https://github.com/smsubrahmannian/Sprint.git sprint; fi",
-				"rm -f -- /srv/runme/" + prefix + ".txt",
-				"crontab -l > mycron", crontab_command,
-				"crontab mycron"]
+                crontab_command = "echo '* * * * * python sprint/sprint2_step2.py "+prefix+"' >> mycron"
+                commands = ["if [ ! -d 'sprint' ]; then git clone https://github.com/smsubrahmannian/Sprint.git sprint; fi",
+                                        "rm -f -- /srv/runme/" + prefix + ".txt",
+                                        "crontab -l > mycron", crontab_command,
+                                        "crontab mycron"]
 
-	for command in commands:
-		print command
-		stdin, stdout, stderr = ssh.exec_command(command)
-		out, err = (stdout.read(), stderr.read())
-		if len(out) > 0:
-			print out
-		if len(err) > 0:
-			print err
-		time.sleep(1)
+                for command in commands:
+                        print command
+                        stdin, stdout, stderr = ssh.exec_command(command)
+                        out, err = (stdout.read(), stderr.read())
+                        if len(out) > 0:
+                                print out
+                        if len(err) > 0:
+                                print err
+                        time.sleep(1)
 
-	ssh.close()
+                ssh.close()
+        except: print("Connection error, check the pem file and/or server")
 
-deploy(key_path, host, prefix)
 
+#---------------------------------------------------------------------------#
+        
+# check if all variables are provided
+if len(sys.argv)==4:
+        key_path = sys.argv[1]
+        host = sys.argv[2]
+        prefix = sys.argv[3]
+        user = 'ec2-user' # ML: Isn't this going to be 'testtest'?
+        deploy(key_path, host, prefix)
+else:
+        print("Missing parameters, please check!")
+
+        
 ## EOF ##
