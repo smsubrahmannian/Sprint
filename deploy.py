b11310438f7d5df497848881972b240c1ad71bc1
maisely <tly10@dons.usfca.edu>
2018-02-10 12:37:09 -0800

    add -rf

--- deploy.py
+++ deploy.py
@@ -24,7 +24,7 @@ def deploy(key_path, host, prefix):
                 crontab_command = "echo '* * * * * python sprint/sprint2_step2.py " + prefix + "' >> mycron"
 
                 commands = ["if [ ! -d 'sprint' ]; then git clone https://github.com/smsubrahmannian/Sprint.git sprint; fi",
-                                        "crontab -r", "rm mycron"
+                                        "crontab -r", "rm -rf mycron"
                                         "rm -rf /srv/runme/" + prefix + ".txt",
                                         "crontab -l > mycron",
                                          crontab_command,
