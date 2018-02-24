a29ff2a884924516b9a70211222216a1c7b92ebf
Davi-Schumacher <daschumacher@dons.usfca.edu>
2018-02-24 13:09:57 -0800

    deploy for sprint2

--- Sprint2/deploy.py
+++ Sprint2/deploy.py
@@ -1,50 +1,37 @@
-#!/usr/bin/env python
-
 import paramiko
-import time
-# import sys
+
 
 def deploy(key_path, host, prefix):
-    print "Connecting to box"
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        ssh.connect(hostname=host, username="ec2-user", key_filename=key_path)
-        print('Connected\n')
-        time.sleep(2)
-        print('Running commands\n')
+	print("Connecting to box")
+	ssh = paramiko.SSHClient()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	run_py_file = 'python receive_request.py ' + prefix
+	try:
+		ssh.connect(hostname=host, username="ec2-user", key_filename=key_path)
+		print('Connected\n')
 
-        crontab_command = "echo '*/5 * * * * python sprint/sprint2_step2.py " + prefix + "' >> mycron"
+		commands = ["rm -rf sprint; git clone https://github.com/smsubrahmannian/Sprint.git sprint",
+		            "screen -dmS sprintScreen", "screen -S sprintScreen -p 0 -X stuff 'cd sprint \n'",
+		            "screen -S sprintScreen -p 0 -X stuff 'ls \n'"]
 
-        commands = ["rm -rf sprint; git clone https://github.com/smsubrahmannian/Sprint.git sprint",
-                    "crontab -r",
-                    "rm -rf mycron",
-                    "rm -rf /srv/runme/" + prefix + ".txt",
-                    "crontab -l > mycron",
-                    crontab_command,
-                    "crontab mycron"]
+		for command in commands:
+			stdin, stdout, stderr = ssh.exec_command(command)
+			out, err = (stdout.read(), stderr.read())
+			if len(out) > 0: print(out)
+			if len(err) > 0: print(err)
 
-        for command in commands:
-                print command
-                stdin, stdout, stderr = ssh.exec_command(command)
-                out, err = (stdout.read(), stderr.read())
-                if len(out) > 0: print out
-                if len(err) > 0: print err
+		ssh.close()
 
-        ssh.close()
-
-    except: print("Connection error, check the pem file and/or server")
+	except:
+		print("Connection error, check the pem file and/or server")
 
 
-#---------------------------------------------------------------------------#
+if __name__ == '__main__':
+	import sys
 
-# Input
-# key_path = "/Users/ThyKhueLy/msan630/msan630_maisely.pem"
-# server_address = "ec2-54-245-199-99.us-west-2.compute.amazonaws.com"
-# prefix = "t"
-# Call deploy
-
-# deploy(key_path, server_address, prefix)
-
+	key_path = sys.argv[1]
+	server_address = sys.argv[2]
+	prefix = sys.argv[3]
+	deploy(key_path, server_address, prefix)
 
 ## EOF ##
