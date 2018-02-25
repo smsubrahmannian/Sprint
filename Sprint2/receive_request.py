87d3581234baef22bbe6d4391354d27834516cc6
maisely <tly10@dons.usfca.edu>
2018-02-24 16:18:00 -0800

    tweaked receive_request

--- Sprint2/receive_request.py
+++ Sprint2/receive_request.py
@@ -2,24 +2,13 @@ import sys
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from flask import Flask, request
+import time
 from create_log import *
 from proc_json import *
 
 #-----------------------------------------------------
 # Flask App
 #-----------------------------------------------------
-
-def create_timed_rotating_log(raw_file_path):
-    logger = logging.getLogger("Rotating Log")
-    logger.setLevel(logging.INFO)
-
-    handler = TimedRotatingFileHandler(raw_file_path,
-                                       when="s",
-                                       interval=2,
-                                       backupCount=0)
-    logger.addHandler(handler)
-    with open(raw_file_path, 'r') as raw:
-        logger.info(raw.read())
 
 
 app = Flask(__name__)
@@ -32,11 +21,11 @@ def receive_request():
     try:
         content = request.data
         if content is not None:
-            create_timed_rotating_log(PATH + PREFIX + "/Raw.txt")
-            write_log(PREFIX, content, PATH)  # call create_log
+            LOGGER.info(content)
+            # write_log(PREFIX, content, PATH)  # call create_log
             # write_json(PREFIX, PATH) # call proc_json
 
-    except Exception as e: print(e) 
+    except Exception as e: print(e)
     return "Received data!"
 
 
@@ -44,4 +33,13 @@ if __name__ == '__main__':
 
     PATH = "/Users/ThyKhueLy/msan603/00-sprint/"
     PREFIX = sys.argv[1]
+    log_filename = PATH + PREFIX + "/Raw.txt"
+    LOGGER = logging.getLogger('MyLogger')
+    LOGGER.setLevel(logging.INFO)
+    handler = TimedRotatingFileHandler(log_filename,
+                                       when="s",
+                                       interval=30,
+                                       backupCount=0)
+    handler.suffix = "%Y%m%d-%H%M%s.log"
+    LOGGER.addHandler(handler)
     app.run(host='0.0.0.0', port=8080, debug=True)
