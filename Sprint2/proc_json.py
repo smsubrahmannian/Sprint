fe38e54a76b3bdebe87c40cecc207a9567ea9eba
maisely <tly10@dons.usfca.edu>
2018-02-27 11:18:10 -0800

    added strip

--- Sprint2/proc_json.py
+++ Sprint2/proc_json.py
@@ -25,8 +25,8 @@ def proc_line(line):
     json_text = json.loads(line)
 
     try: # extract name & age
-        name = json_text['name']
-        age = json_text['prop']['age']
+        name = json_text['name'].strip()
+        age = json_text['prop']['age'].strip()
 
         # check if name / age exists and with a correct format
         if name != '' and age != '':
